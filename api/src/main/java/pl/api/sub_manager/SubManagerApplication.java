4191e573a479f9366060eacd0418c44e485c4591
Vigz <gabrielgaleza@gmail.com>
2025-01-23 19:38:44 +0100

    feat: Added simple Spring Boot RestAPI project

---
+++ api/src/main/java/pl/api/sub_manager/SubManagerApplication.java
@@ -0,0 +1,13 @@
+package pl.api.sub_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubManagerApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/pl/api/sub_manager/SubManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.api.sub_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
