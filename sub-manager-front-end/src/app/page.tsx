d41ab29a8d89f45e92915d47974ab08d48dab918
Vigz <gabrielgaleza@gmail.com>
2025-02-11 02:26:10 +0100

    fix: made a small ajust for front-end for connecting to the api by env

--- sub-manager-front-end/src/app/page.tsx
+++ sub-manager-front-end/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const [data, setData] = useState<string | null>(null)
 
   useEffect(() => {
-    fetch("http://localhost:8080")
+    fetch(process.env.API_URL || "")
       .then(res => res.text())
       .then((data: string) => setData(data)) 
       .catch((err: Error) => console.log(err))
